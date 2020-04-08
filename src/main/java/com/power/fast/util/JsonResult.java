05924605dbc946927af468cea6155ecfd850778d
hou yu fei <43112607+Github9564@users.noreply.github.com>
2020-04-08 15:52:03 +0800

    feat(添加返回值): 定义返回数据
    1.添加常用的string 常用工具类
    2.添加返回值对象

---
+++ src/main/java/com/power/fast/util/JsonResult.java
@@ -0,0 +1,141 @@
+package com.power.fast.util;
+
+import java.util.HashMap;
+
+/**
+ * @author fei
+ * @title: JsonResult
+ * @projectName power-fast
+ * @description: 返回数据
+ * @date 2020/4/08 18:05
+ */
+public class JsonResult extends HashMap<String, Object> {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 状态码
+     */
+    public static final String CODE_TAG = "code";
+
+    /**
+     * 返回内容
+     */
+    public static final String MSG_TAG = "msg";
+
+    /**
+     * 数据对象
+     */
+    public static final String DATA_TAG = "data";
+
+    /**
+     * 初始化一个新创建的 AjaxResult 对象，使其表示一个空消息。
+     */
+    public JsonResult() {
+    }
+
+    /**
+     * 初始化一个新创建的 AjaxResult 对象
+     *
+     * @param code 状态码
+     * @param msg  返回内容
+     */
+    public JsonResult(int code, String msg) {
+        super.put(CODE_TAG, code);
+        super.put(MSG_TAG, msg);
+    }
+
+    /**
+     * 初始化一个新创建的 AjaxResult 对象
+     *
+     * @param code 状态码
+     * @param msg  返回内容
+     * @param data 数据对象
+     */
+    public JsonResult(int code, String msg, Object data) {
+        super.put(CODE_TAG, code);
+        super.put(MSG_TAG, msg);
+        if (StringUtils.isNotNull(data)) {
+            super.put(DATA_TAG, data);
+        }
+    }
+
+    /**
+     * 返回成功消息
+     *
+     * @return 成功消息
+     */
+    public static JsonResult success() {
+        return JsonResult.success("操作成功");
+    }
+
+    /**
+     * 返回成功数据
+     *
+     * @return 成功消息
+     */
+    public static JsonResult success(Object data) {
+        return JsonResult.success("操作成功", data);
+    }
+
+    /**
+     * 返回成功消息
+     *
+     * @param msg 返回内容
+     * @return 成功消息
+     */
+    public static JsonResult success(String msg) {
+        return JsonResult.success(msg, null);
+    }
+
+    /**
+     * 返回成功消息
+     *
+     * @param msg  返回内容
+     * @param data 数据对象
+     * @return 成功消息
+     */
+    public static JsonResult success(String msg, Object data) {
+        return new JsonResult(com.power.fast.util.HttpStatus.SUCCESS, msg, data);
+    }
+
+    /**
+     * 返回错误消息
+     *
+     * @return
+     */
+    public static JsonResult error() {
+        return JsonResult.error("操作失败");
+    }
+
+    /**
+     * 返回错误消息
+     *
+     * @param msg 返回内容
+     * @return 警告消息
+     */
+    public static JsonResult error(String msg) {
+        return JsonResult.error(msg, null);
+    }
+
+    /**
+     * 返回错误消息
+     *
+     * @param msg  返回内容
+     * @param data 数据对象
+     * @return 警告消息
+     */
+    public static JsonResult error(String msg, Object data) {
+        return new JsonResult(HttpStatus.ERROR, msg, data);
+    }
+
+    /**
+     * 返回错误消息
+     *
+     * @param code 状态码
+     * @param msg  返回内容
+     * @return 警告消息
+     */
+    public static JsonResult error(int code, String msg) {
+        return new JsonResult(code, msg, null);
+    }
+}
