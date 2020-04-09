7c64a77040a53f64f516959f2c40991b35557e5d
hou yu fei <43112607+Github9564@users.noreply.github.com>
2020-04-09 10:15:59 +0800

    fix(无法输出图片): swagger无法输出图片
    swagger无法输出验证码图片，但是他也不报错。

--- src/main/java/com/power/fast/modules/sys/controller/SysLoginController.java
+++ src/main/java/com/power/fast/modules/sys/controller/SysLoginController.java
@@ -4,12 +4,14 @@ import com.power.fast.modules.sys.service.SysCaptchaService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.commons.io.IOUtils;
 
 import javax.imageio.ImageIO;
+import javax.print.attribute.standard.MediaSize;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
@@ -29,10 +31,11 @@ public class SysLoginController {
 
     @Autowired
     private SysCaptchaService sysCaptchaService;
+
     /**
      * 验证码
      */
-    @GetMapping("/captcha.jpg")
+    @GetMapping(value = "/captcha.jpg", produces = MediaType.IMAGE_JPEG_VALUE)
     @ApiOperation("获取验证码")
     public void captcha(HttpServletResponse response, String uuid) throws IOException {
         response.setHeader("Cache-Control", "no-store, no-cache");
